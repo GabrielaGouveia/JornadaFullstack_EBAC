168c3a9235d61c39ea40655c35e76c8a1d3aaf6f
GabrielaGouveia <gabii77leone@gmail.com>
2023-06-19 16:17:06 -0300

    Layout para mobile

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import {collection, getDocs} from 'firebase/firestore/lite';
 
 function App() {
 
+    let maxHeight;
+    if(window.innerHeight<= 800){
+      maxHeight=window.innerHeight
+    }
+
 
    const [video,setVideos]= useState([])
 
@@ -28,7 +33,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" style={{maxHeight:maxHeight + "px"}}>
       <div className='app__videos'>
         {video.map((item)=>{
           return( 
@@ -44,6 +49,8 @@ function App() {
             
             
             />
+
+            
 
           )
 
